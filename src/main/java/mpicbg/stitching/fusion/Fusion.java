da2e5290a1f632b89ee77005bd9fea67739aebaa
Mark Hiner <hiner@wisc.edu>
2014-04-05 13:43:55 -0500

    Fusion: reduce overlap fusion runtime
    The previous overlap fusion mechanism was, for each pixel position,
    check each image to see if that image contains the current position. If
    so, add its value to the fusion for that position.
    
    Certainly we want to fuse one output position at a time to avoid a huge
    number of PixelFusion objects, but the #pixels X #images complexity was
    killing performance.
    
    We want to do as little work as possible during the fusion process. So,
    using the ClassifiedRegion class, fusion with overlap will now spend
    some processing time up front to determine each non-overlapping
    sub-region, using the associated images as the classification values.
    
    For example, if image 1 and 2 overlap partially, we will come up with
    one or more "image 1" regions, one or more "image 2" regions, and one
    "image 1 + 2" region.
    
    Positional iteration is then driven by these regions, as each knows
    exactly which images to pull values from when fusing.
    
    Time reductions will be seen as a function of the number of images being
    fused. This was benchmarked with a dataset with 153 images, which
    resulted in a 10X improvement in performance.

--- src/main/java/mpicbg/stitching/fusion/Fusion.java
+++ src/main/java/mpicbg/stitching/fusion/Fusion.java
@@ -10,7 +10,11 @@ import ij.io.FileSaver;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.Stack;
 import java.util.Vector;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -37,6 +41,8 @@ import mpicbg.imglib.type.numeric.integer.UnsignedShortType;
 import mpicbg.imglib.type.numeric.real.FloatType;
 import mpicbg.models.InvertibleBoundable;
 import mpicbg.models.NoninvertibleModelException;
+import stitching.Interval;
+import stitching.ClassifiedRegion;
 import stitching.utils.CompositeImageFixer;
 
 /**
@@ -276,23 +282,77 @@ public class Fusion
 	{
 		final int numDimensions = output.getNumDimensions();
 		final int numImages = input.size();
-		long imageSize = output.getDimension( 0 );
-		
-		for ( int d = 1; d < output.getNumDimensions(); ++d )
-			imageSize *= output.getDimension( d );
-		
-		IJ.showProgress( 0 );
+		long size = output.getDimension( 0 );
+
+		for (int d = 1; d < output.getNumDimensions(); ++d) {
+			size *= output.getDimension(d);
+		}
+
+		Stack<ClassifiedRegion> rawTiles = new Stack<ClassifiedRegion>();
 
 		final int[][] max = new int[ numImages ][ numDimensions ];
-		for ( int i = 0; i < numImages; ++i )
-			for ( int d = 0; d < numDimensions; ++d )
-				max[ i ][ d ] = input.get( i ).getImage().getDimension( d ) - 1; 
+		final float[][] min = new float[ numImages ][ numDimensions ];
+		for ( int i = 0; i < numImages; ++i ){
+				transform.get(i).applyInPlace(min[i]);
+				ClassifiedRegion shape = new ClassifiedRegion(numDimensions);
+				shape.addClass(i);
+			for ( int d = 0; d < numDimensions; ++d ) {
+				max[ i ][ d ] = input.get( i ).getImage().getDimension( d ) - 1;
+				Interval ival = new Interval((int) min[i][d], (int) (min[i][d] + input.get(i).getImage().getDimension(d)));
+				// Build our list of positions
+				shape.set(ival, d);
+			}
+			rawTiles.push(shape);
+		}
+
+		// Set of placed tiles. Goal is to move all of the known positions
+		// to this set, creating new regions as needed such that there is no
+		// overlap between regions. Then use this set to drive iteration and
+		// fusion.
+		final Set<ClassifiedRegion> placedTiles = new HashSet<ClassifiedRegion>();
+
+		// Process each position. We need to look through all placed regions and
+		// if we find an intersection, create a new set of regions, add them
+		// to the appropriate sets, and continue
+		while (!rawTiles.isEmpty()) {
+			// Get the next tile to process
+			ClassifiedRegion queryTile = rawTiles.pop();
+			ClassifiedRegion toRemove = null;
+			// Iterate over all placed shapes.
+			for (ClassifiedRegion placedTile : placedTiles) {
+				if (queryTile.intersects(placedTile)) {
+					// can't remove while iterating
+					toRemove = placedTile;
+					// The first time we find an overlapping between tiles, we split the
+					// two tiles into tile components and place them in the appropriate
+					// lists. Then quit this loop (as our query shape is no longer valid)
+					// and continue the process
+					splitOverlappingRegions(placedTiles, rawTiles, queryTile, placedTile);
+					break;
+				}
+			}
+			// If we found an intersection, the query tile and placed tile were broken
+			// down and should be discarded
+			if (toRemove != null) {
+				placedTiles.remove(toRemove);
+			}
+			// No intersections found, so just place the tile and continue.
+			else {
+				placedTiles.add(queryTile);
+			}
+		}
+
+		final List<ClassifiedRegion> tiles = new ArrayList<ClassifiedRegion>(placedTiles);
+
+		IJ.showProgress( 0 );
+
 		
 		// run multithreaded
 		final AtomicInteger ai = new AtomicInteger(0);					
         final Thread[] threads = SimpleMultiThreading.newThreads();
 
-        final Vector<Chunk> threadChunks = SimpleMultiThreading.divideIntoChunks( imageSize, threads.length );
+        final Vector<Chunk> threadChunks = SimpleMultiThreading.divideIntoChunks( tiles.size(), threads.length );
+        final long positionsPerThread = size / threadChunks.size();
         
         for (int ithread = 0; ithread < threads.length; ++ithread)
             threads[ithread] = new Thread(new Runnable()
@@ -301,14 +361,14 @@ public class Fusion
                 public void run()
                 {
                 	// Thread ID
-                	final int myNumber = ai.getAndIncrement();
+                	final int threadNumber = ai.getAndIncrement();
         
                 	// only the first thread does preview and update the status bar
                 	// this requires no synchronized stuff
-            		long lastDraw = 0;
+            		long[] lastDraw = new long[1];
             		ImagePlus fusionImp = null;
 
-                	if ( displayFusion && myNumber == 0 )
+                	if ( displayFusion && threadNumber == 0 )
                 	{
                 		try
             			{
@@ -323,69 +383,35 @@ public class Fusion
                 	}
 
                 	// get chunk of pixels to process
-                	final Chunk myChunk = threadChunks.get( myNumber );
+                	final Chunk myChunk = threadChunks.get( threadNumber );
                 	final long startPos = myChunk.getStartPosition();
                 	final long loopSize = myChunk.getLoopSize();
                 	
-            		final LocalizableCursor<T> out = output.createLocalizableCursor();
             		final ArrayList<Interpolator<? extends RealType<?>>> in = new ArrayList<Interpolator<? extends RealType<?>>>();
             		
             		for ( int i = 0; i < numImages; ++i )
             			in.add( input.get( i ).createInterpolator() );
             		
-            		final float[][] tmp = new float[ numImages ][ output.getNumDimensions() ];
+            		final int[] outPos = new int[ output.getNumDimensions() ];
+            		final float[] inPos = new float[ output.getNumDimensions() ];
+            		final int[] count = new int[1];
             		final PixelFusion myFusion = fusion.copy();
+            		LocalizableByDimCursor<T> out = output.createLocalizableByDimCursor();
             		
             		try 
             		{
-                		// move to the starting position of the current thread
-            			out.fwd( startPos );
-            			
-                		// do as many pixels as wanted by this thread
-                        for ( long j = 0; j < loopSize; ++j )
-                        {
-            				out.fwd();
+						// Process each tile index assigned to this thread
+						for (long tileIndex = startPos; tileIndex < startPos + loopSize; tileIndex++)
+						{
+							//TODO we're using integer indices... might need to use floats + step values for subpixel accuracy?
+							// Current tile
+							ClassifiedRegion tile = tiles.get((int) tileIndex);
+							// For each position in this tile, fuse its pixels across the appropriate images
+							// NB: recursion is necessary because there are an arbitrary number of dimensions in the tile
+							processTile(tile, 0, myFusion, transform, in, out, inPos, outPos,
+								threadNumber, count, lastDraw, fusionImp);
 
-            				// just thread 0
-            				if ( myNumber == 0 )
-            				{
-            					// just every 10000'th pixel
-            					if ( j % 10000 == 0 )
-            					{
-                    				lastDraw = drawFusion( lastDraw, fusionImp );
-            						IJ.showProgress( (double)j / (double)loopSize );
-            					}
-            				}
-            				
-            				// get the current position in the output image
-            				for ( int d = 0; d < numDimensions; ++d )
-            				{
-            					final float value = out.getPosition( d ) + offset[ d ];
-            					
-            					for ( int i = 0; i < numImages; ++i )
-            						tmp[ i ][ d ] = value;
-            				}
-            				
-            				// transform and compute output value
-            				myFusion.clear();
-            				
-            				// loop over all images for this output location
-A:        					for ( int i = 0; i < numImages; ++i )
-        					{
-        						transform.get( i ).applyInverseInPlace( tmp[ i ] );
-            	
-        						// test if inside
-        						for ( int d = 0; d < numDimensions; ++d )
-        							if ( tmp[ i ][ d ] < 0 || tmp[ i ][ d ] > max[ i ][ d ] )
-        								continue A;
-        						
-        						in.get( i ).setPosition( tmp[ i ] );			
-        						myFusion.addValue( in.get( i ).getType().getRealFloat(), i, tmp[ i ] );
-        					}
-            				
-            				// set value
-    						out.getType().setReal( myFusion.getValue() );
-                        }
+						}
             		} 
             		catch ( NoninvertibleModelException e ) 
             		{
@@ -396,9 +422,234 @@ A:        					for ( int i = 0; i < numImages; ++i )
                     if ( fusionImp != null )
                     	fusionImp.hide();
                 }
-            });
+
+				/**
+				 * Helper method to fuse all the positions of a given
+				 * {@link ClassifiedRegion}. Since we do not know the dimensionality of
+				 * the region, we recurse over each position of each dimension. The tail
+				 * step of each descent iterates over all the images (classes) of the
+				 * given region, fusing the pixel values at the current position of each
+				 * associated image. This final value is then set in the output.
+				 */
+				private void processTile(ClassifiedRegion r, int depth, PixelFusion myFusion,
+					ArrayList<InvertibleBoundable> transform,
+					ArrayList<Interpolator<? extends RealType<?>>> in,
+					LocalizableByDimCursor<T> out, float[] inPos, int[] outPos,
+					int threadNumber, int[] count, long[] lastDraw,
+					ImagePlus fusionImp) throws NoninvertibleModelException
+				{
+					if (depth < r.size()) {
+						Interval d = r.get(depth);
+						// The intervals of the given region define the bounds of iteration
+						// So we are recursively defining a nested iteration order to cover
+						// each position of the region
+						for (int i=d.start(); i<d.end(); i++) {
+							// The position array will be used to set the in and out positions.
+							// It specifies where we are in the output image
+							outPos[depth] = i;
+							// Recurse to the next depth (dimension)
+							processTile(r, depth+1, myFusion, transform, in, out, inPos,
+								outPos, threadNumber, count, lastDraw, fusionImp);
+						}
+						return;
+					}
+
+					// compute fusion for this position
+					myFusion.clear();
+					// These are the image indices associated with this region
+					final int[] indices = r.classArray();
+
+					// Loop over the images in this region
+					for (int index=0; index<indices.length; index++) {
+						// Get the positions for the current image
+						for (int d=0; d<r.size(); d++) {
+							inPos[d] = outPos[d] + offset[d];
+						}
+						// Transform to get input position
+						transform.get(indices[index]).applyInverseInPlace(inPos);
+						in.get(indices[index]).setPosition(inPos);
+						// fuse
+						myFusion.addValue(in.get(indices[index]).getType().getRealFloat(),
+							indices[index], inPos);
+					}
+
+					// set value
+					out.setPosition(outPos);
+					out.getType().setReal(myFusion.getValue());
+					count[0]++;
+
+					// Display progress if on thread 0
+  				if ( threadNumber == 0 )
+  				{
+  					// just every 10000'th pixel
+  					if ( count[0] % 10000 == 0 )
+  					{
+          				lastDraw[0] = drawFusion( lastDraw[0], fusionImp );
+  						IJ.showProgress( (double)count[0] / (double)positionsPerThread );
+  					}
+  				}
+				}
+
+			});
         
         SimpleMultiThreading.startAndJoin( threads );        
+	}
+
+	/**
+	 * Takes two overlapping regions and deconstructs them into a set of non-overlapping regions.
+	 * Each resulting region gains the classification(s) of its parent(s). Parents are differentiated
+	 * by "query" and "placed" - where "placed" is assumed to come from the {@code placedTiles} set,
+	 * and thus will not overlap with any other tile in that set. The "query" tile is what we are
+	 * currently investigating for overlap, so any children of the query tile must be added back to
+	 * the set of tiles that will be used for further overlap checks.
+	 */
+	private static void splitOverlappingRegions(Set<ClassifiedRegion> placedTiles,
+		Stack<ClassifiedRegion> rawTiles, ClassifiedRegion queryTile, ClassifiedRegion placedTile)
+	{
+		// First we do an explicit simple check to see if the tiles are the same:
+		if (sameTile(queryTile, placedTile)) {
+			// NB: the original place tile will be removed from the list, so we create a new tile
+			// and add all indices.
+			ClassifiedRegion newTile = new ClassifiedRegion(queryTile);
+			newTile.addAllClasses(placedTile);
+			placedTiles.add(newTile);
+			return;
+		}
+
+		// Tiles are different but overlapping. So we need to identify the start and end points of
+		// all potential sub tiles, for each dimension.
+		List<Interval>[] allIntervals = new List[queryTile.size()];
+		for (int i=0; i<allIntervals.length; i++) {
+			List<Integer> points = new ArrayList<Integer>();
+			List<Interval> intervals = new ArrayList<Interval>();
+			points.add(queryTile.get(i).start());
+			points.add(queryTile.get(i).end());
+			points.add(placedTile.get(i).start());
+			points.add(placedTile.get(i).end());
+			// Which tiles the points belong to doesn't matter. We just need to make intervals of the adjacent points
+			Collections.sort(points);
+			for (int j=0; j<points.size() - 1; j++) {
+				if (!points.get(j).equals(points.get(j+1))) {
+					intervals.add(new Interval(points.get(j), points.get(j+1)));
+				}
+			}
+			allIntervals[i] = intervals;
+		}
+
+		// Now that we know our potential intervals, we need to enumerate a list of all regions constructable from those intervals
+		List<ClassifiedRegion> allRegions = new ArrayList<ClassifiedRegion>();
+		int[] pos = new int[allIntervals.length];
+		buildAllRegions(allRegions, allIntervals, pos, 0);
+
+		ClassifiedRegion overlappedRegion = null;
+
+		// Update the classification of each sub-region. Possibilities include: 1 parent, both parents, or no parents (in which case
+		// the region is discarded)
+		for (ClassifiedRegion newRegion : allRegions) {
+			boolean matchedQueried = false;
+			boolean matchedPlaced = false;
+			// If there's an intersection in either case here, it's because the new region
+			// is a subset of one of the two parent tiles. Thus it should inherit all the
+			// index labels of the parent(s).
+			if (queryTile.intersects(newRegion, true)) {
+				newRegion.addAllClasses(queryTile);
+				matchedQueried = true;
+			}
+			if (placedTile.intersects(newRegion, true)) {
+				newRegion.addAllClasses(placedTile);
+				matchedPlaced = true;
+			}
+
+			if (!matchedQueried && matchedPlaced) {
+				// This is a sub-tile of a placed tile only. Thus we know it does not intersect
+				// with any other placed tile, so we can add it back to the placed set.
+				placedTiles.add(newRegion);
+			}
+			else if (matchedQueried) {
+				// This is either a) a new overlapping sub-tile or b) a pure queried sub-tile.
+				// In either case, since we short-circuited the query after finding our first overlap,
+				// the new tile needs to be tested against other placed tiles. So add it
+				// back to the raw list.
+				rawTiles.push(newRegion);
+
+				// Only one region will be created that is overlapped by both original regions
+				// It can then be used to reduce the area of the other regions
+				if (matchedPlaced) {
+					overlappedRegion = newRegion;
+				}
+
+				// Regions that didn't match either parent are discarded
+			}
+		}
+
+		// Now we need to shrink all the intervals that were not overlapped by both
+		// the query and placed regions. This ensures that there will be no potential
+		// overlap between the new sub-regions (so each position is claimed by a single region)
+		// and that the maximum number of classes will be applied when possible.
+		for (ClassifiedRegion region : allRegions) {
+			if (overlappedRegion != null && region != overlappedRegion) {
+				for (int i=0; i<region.size(); i++) {
+					Interval primeInterval = overlappedRegion.get(i);
+					Interval secondaryInterval = region.get(i);
+					// Test the start point to ensure the two are not the same interval
+					if (primeInterval.start() != secondaryInterval.start()) {
+						if (primeInterval.start() == secondaryInterval.end()) {
+							// Move the secondary interval's end down 1, so that their border
+							// becomes owned exclusively by the prime interval
+							secondaryInterval.setEnd(secondaryInterval.end() - 1);
+						}
+						else if (primeInterval.end() == secondaryInterval.start()) {
+							// Move the secondary interval's start up 1, so that their border
+							// becomes owned exclusively by the prime interval
+							secondaryInterval.setStart(secondaryInterval.start() + 1);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Helper method to check the special case when two regions are identical.
+	 */
+	private static boolean sameTile(ClassifiedRegion tile1, ClassifiedRegion tile2) {
+		boolean same = true;
+		// Check the start and end points of each dimension
+		for (int i=0; same && i<tile1.size(); i++) {
+			same =
+				tile1.get(i).start() == tile2.get(i).start() &&
+					tile1.get(i).end() == tile2.get(i).end();
+		}
+		return same;
+	}
+
+	/**
+	 * Recursive method to construct all possible {@link ClassifiedRegion}
+	 * combinations from an arbitrary number of {@link Interval} lists. One list
+	 * is required for each dimension of the final regions. The {@code allRegions}
+	 * list is then populated with these combinations.
+	 */
+	private static void buildAllRegions(List<ClassifiedRegion> allRegions,
+		List<Interval>[] allIntervals, int[] ivalIndices, int depth)
+	{
+		if (depth != ivalIndices.length) {
+			for (int i = 0; i < allIntervals[depth].size(); i++) {
+				buildAllRegions(allRegions, allIntervals, ivalIndices, depth + 1);
+				// increment the index at this position in the intervals array
+				ivalIndices[depth]++;
+			}
+			ivalIndices[depth] = 0;
+			return;
+		}
+
+		// Build a new region using the current specified indices
+		ClassifiedRegion region = new ClassifiedRegion(allIntervals.length);
+
+		for (int i=0; i<allIntervals.length; i++) {
+			region.set(allIntervals[i].get(ivalIndices[i]), i);
+		}
+
+		allRegions.add(region);
 	}
 
 	/**
