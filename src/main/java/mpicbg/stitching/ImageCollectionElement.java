07b8c02289fbb34faa5a2031722cdbf9c22c55c8
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2017-03-09 12:57:47 -0500

    hack-fixed the unhappy situation that unspecified third dimension is interpreted as time

--- src/main/java/mpicbg/stitching/ImageCollectionElement.java
+++ src/main/java/mpicbg/stitching/ImageCollectionElement.java
@@ -1,16 +1,16 @@
 package mpicbg.stitching;
 
-import ij.IJ;
-import ij.ImagePlus;
-
 import java.io.File;
 
+import ij.IJ;
+import ij.ImagePlus;
+import ij.io.Opener;
 import loci.plugins.BF;
 import loci.plugins.in.ImporterOptions;
 import mpicbg.models.Model;
 import stitching.utils.Log;
 
-public class ImageCollectionElement 
+public class ImageCollectionElement
 {
 	final File file;
 	ImagePlus imp = null;
@@ -18,52 +18,52 @@ public class ImageCollectionElement
 	Model<?> model;
 	int dimensionality;
 	boolean virtual = false;
-	
+
 	//2d or 3d offset
-	float[] offset;	
-	
+	float[] offset;
+
 	//2d or 3d size if image
 	int[] size;
-	
+
 	public ImageCollectionElement( final File file, final int index )
 	{
 		this.file = file;
-		this.index = index;		
+		this.index = index;
 	}
-	
+
 	public void setOffset( final float[] offset ) { this.offset = offset; }
 	public float[] getOffset() { return offset; }
 	public float getOffset( final int dim ) { return offset[ dim ]; }
-	
+
 	public int[] getDimensions() { return size; }
 	public int getDimension( final int dim ) { return size[ dim ]; }
-	
+
 	public int getIndex() { return index; }
-	
+
 	public void setModel( final Model<?> model ) { this.model = model; }
 	public Model<?> getModel() { return model; }
-	
+
 	public void setDimensionality( final int dimensionality ) { this.dimensionality = dimensionality; }
 	public int getDimensionality() { return dimensionality; }
-	
+
 	public File getFile() { return file; }
 	public boolean isVirtual() { return virtual; }
-	
+
 	/**
 	 * Used by the multi-series stitching
-	 * 
+	 *
 	 * @param imp - the ImagePlus of this series
 	 */
-	public void setImagePlus( final ImagePlus imp ) 
-	{ 
-		this.imp = imp; 
-		
+	public void setImagePlus( final ImagePlus imp )
+	{
+		this.imp = imp;
+
 		if ( imp.getNSlices() == 1 )
 			size = new int[] { imp.getWidth(), imp.getHeight() };
 		else
-			size = new int[] { imp.getWidth(), imp.getHeight(), imp.getNSlices() };	
+			size = new int[] { imp.getWidth(), imp.getHeight(), imp.getNSlices() };
 	}
-	
+
 	public ImagePlus open( final boolean virtual )
 	{
 		if ( imp != null && this.isVirtual() == virtual )
@@ -75,57 +75,72 @@ public class ImageCollectionElement
 		// are loaded must be made in multiple places in the code.
 		if ( imp != null )
 			imp.close();
-		
+
 		this.virtual = virtual;
-		
-		try 
+
+		try
 		{
 			if ( !file.exists() )
 			{
 				Log.error( "Cannot find file: '" + file + "' - abort stitching." );
 				return null;
 			}
-			
-			ImporterOptions options = new ImporterOptions();
+
+			final ImporterOptions options = new ImporterOptions();
 			options.setId( file.getAbsolutePath() );
 			options.setSplitChannels( false );
 			options.setSplitTimepoints( false );
 			options.setSplitFocalPlanes( false );
 			options.setAutoscale( false );
 			options.setVirtual( virtual );
-			
+
 			final ImagePlus[] imp;
-			
+
 			if ( virtual )
 				imp = BF.openImagePlus( options );
 			else
-				imp = BF.openImagePlus( file.getAbsolutePath() ); // this worked, so we keep it (altough both should be the same)
-			
+			{
+				final ImagePlus impIj = new Opener().openImage( file.getAbsolutePath() );
+				if ( impIj != null )
+				{
+					IJ.log( "Used ImageJ1 tiff opener." );
+					imp = new ImagePlus[]{ impIj };
+				}
+				else
+					imp = BF.openImagePlus( file.getAbsolutePath() ); // this worked, so we keep it (altough both should be the same)
+			}
+
 			if ( imp.length > 1 )
 			{
 				Log.error( "LOCI does not open the file '" + file + "'correctly, it opens the image and splits it - maybe you should convert all input files first to TIFF?" );
-				
-				for ( ImagePlus i : imp )
+
+				for ( final ImagePlus i : imp )
 					i.close();
-				
+
 				return null;
 			}
 			if ( imp[ 0 ].getNSlices() == 1 )
-				size = new int[] { imp[ 0 ].getWidth(), imp[ 0 ].getHeight() };
+				if ( imp[ 0 ].getNFrames() > 1 )
+				{
+					imp[ 0 ].setDimensions( imp[ 0 ].getNChannels(), imp[ 0 ].getNFrames(), 1 );
+					size = new int[] { imp[ 0 ].getWidth(), imp[ 0 ].getHeight(), imp[ 0 ].getNSlices() };
+				}
+				else
+					size = new int[] { imp[ 0 ].getWidth(), imp[ 0 ].getHeight() };
 			else
 				size = new int[] { imp[ 0 ].getWidth(), imp[ 0 ].getHeight(), imp[ 0 ].getNSlices() };
-			
+
 			this.imp = imp[ 0 ];
 			return this.imp;
-		} 
-		catch ( Exception e ) 
+		}
+		catch ( final Exception e )
 		{
 			Log.error( "Cannot open file '" + file + "': " + e );
 			return null;
 		}
 	}
 
-	public void close() 
+	public void close()
 	{
 		imp.close();
 		imp = null;
